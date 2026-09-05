1ea74d04260b2dd654f98dc284bb877966a722d5
ZeHeng1102 <zhlim-wp24@student.tarc.edu.my>
2026-09-05 19:38:31 +0800

    Foar main

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'core/theme.dart';
-import 'services/auth_service.dart';
 import 'services/delay_alert_service.dart';
 import 'services/notification_service.dart';
 import 'services/ride_detection_service.dart';
 import 'services/supabase_service.dart';
-import 'shared_widgets/app_shell.dart';
 import 'modules/auth/screens/auth_gate.dart';
-import 'services/notification_service.dart';
-import 'services/supabase_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
