f6c0e021479ac50559aff5033962a0c2513f5875
ToArngc <ongx-wp24@student.tarc.edu.my>
2026-09-05 22:33:34 +0800

    refresh supa base url

--- lib/services/supabase_service.dart
+++ lib/services/supabase_service.dart
@@ -9,8 +9,8 @@ class SupabaseService {
 
   static Future<void> initialize() async {
     await Supabase.initialize(
-      url: 'https://pnibgdjxopmvesyklcjt.supabase.co',
-      anonKey: 'sb_publishable_-FfWQTblsFvucpl2uVdYgw_vf-W9y1e',
+      url: 'https://pnibgdjxopmvesyklcjt.supabase.co/rest/v1/',
+      anonKey: 'sb_publishable_-FfWQTblsFvucpl2uVdYgw_vf-W9y1e', 
     );
     client = Supabase.instance.client;
   }
